5907d3975f13d62d53e417ca17d4a9566c007f2a
Jay Zhang <wangyoucao577@gmail.com>
2019-06-05 00:18:16 -0700

    add args and some logs

--- traffic_updater/osrm_traffic_updater.py
+++ traffic_updater/osrm_traffic_updater.py
@@ -1,6 +1,8 @@
 import sys
-import time
 sys.path.append('gen-py')
+
+import time
+import argparse
 
 from proxy import ProxyService
 
@@ -8,6 +10,22 @@ from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TCompactProtocol
+
+class Args:
+    def __init__(self):
+        self.__parser = argparse.ArgumentParser(description='OSRM traffic updater against traffic proxy.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+        # params
+        self.__parser.add_argument('-p', '--port', default=6666, help='traffic proxy listening port', type=int)
+        self.__parser.add_argument('-c', '--ip', default='127.0.0.1', help='traffic proxy ip address', type=str)
+        self.__parser.add_argument('-f', '--csv_file', default='traffic.csv', help='OSRM traffic csv file', type=str)
+
+        # parse
+        self.__args = self.__parser.parse_args()
+
+    def get(self):
+        return self.__args
+
 
 def dump_flows_to_csv(csv_file, flows):
     with open(csv_file, "wb") as writer:
@@ -39,11 +57,13 @@ def dump_flows_to_csv(csv_file, flows):
     
 
 def main():
+    args = Args().get()
+
     # Make socket
-    transport = TSocket.TSocket('localhost', 6666)
+    print "connect traffic_proxy: " + args.ip + ":" + str(args.port)
+    transport = TSocket.TSocket(args.ip, args.port)
 
     # Buffering is critical. Raw sockets are very slow
-    #transport = TTransport.TBufferedTransport(transport)
     transport = TTransport.TFramedTransport(transport)
 
     # Wrap in a protocol
@@ -59,13 +79,15 @@ def main():
     start_time = time.clock()
     #flow = client.getFlowById(991239906)
     #print flow
+    print "getting flows"
     flows = client.getAllFlows()
-    print "flows count: " + str(len(flows))
+    print "got flows count: " + str(len(flows))
     after_get_flow_time = time.clock() 
     print "get flows time used: " + str(after_get_flow_time - start_time) + " seconds"
 
     # Dump to OSRM csv format
-    dump_flows_to_csv("traffic.csv", flows)    
+    print "dump flows to: " + args.csv_file
+    dump_flows_to_csv(args.csv_file, flows)    
 
     end_time = time.clock()
     print "dump csv time used: " + str(end_time - after_get_flow_time) + " seconds"
