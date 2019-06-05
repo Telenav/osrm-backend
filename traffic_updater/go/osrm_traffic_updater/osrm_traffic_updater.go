5756d0499d504a0815775f9f86a3bc88c3822447
Jay Zhang <wangyoucao577@gmail.com>
2019-06-05 17:54:38 +0800

    correct usage and comments

--- traffic_updater/go/osrm_traffic_updater/osrm_traffic_updater.go
+++ traffic_updater/go/osrm_traffic_updater/osrm_traffic_updater.go
@@ -8,13 +8,13 @@ import (
 	"github.com/apache/thrift/lib/go/thrift"
 )
 
-var defaultCtx = context.Background()
 
 func main() {
 
 	var transport thrift.TTransport
 	var err error
 
+	// make socket
 	fmt.Println("connect traffic proxy ")
 	transport, err = thrift.NewTSocket("127.0.0.1:6666")
 	if err != nil {
@@ -22,25 +22,32 @@ func main() {
 		return
 	}
 
+	// Buffering
 	transport, err = thrift.NewTFramedTransportFactoryMaxLength(thrift.NewTTransportFactory(), 1024*1024*1024).GetTransport(transport)
 	if err != nil {
 		fmt.Println("Error get transport:", err)
 		return
 	}
 	defer transport.Close()
-
 	if err := transport.Open(); err != nil {
 		return
 	}
-	iprot := thrift.NewTCompactProtocolFactory().GetProtocol(transport)
-	oprot := thrift.NewTCompactProtocolFactory().GetProtocol(transport)
-	client := proxy.NewProxyServiceClient(thrift.NewTStandardClient(iprot, oprot))
 
+	// protocol encoder&decoder
+	protocol := thrift.NewTCompactProtocolFactory().GetProtocol(transport)
+
+	// create proxy client
+	client := proxy.NewProxyServiceClient(thrift.NewTStandardClient(protocol, protocol))
+
+	// get flows
 	fmt.Println("getting flows")
+	var defaultCtx = context.Background()
 	flows, err := client.GetAllFlows(defaultCtx)
 	if err != nil {
 		fmt.Println("get flows failed:", err)
 		return
 	}
 	fmt.Printf("got flows count: %d\n", len(flows))
+
+	// TODO: dump to csv
 }
