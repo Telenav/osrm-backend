d4cfc36e537a4f6be8dd32f07ea871a63c2ad181
Jay Zhang <wangyoucao577@gmail.com>
2019-06-17 20:04:31 -0700

    chore: improve entrypoint for easy integrated by CI

--- docker-orchestration/osrm-backend/docker-entrypoint.sh
+++ docker-orchestration/osrm-backend/docker-entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
+BUILD_PATH=${BUILD_PATH:="/osrm-build"}
 DATA_PATH=${DATA_PATH:="/osrm-data"}
+OSRM_EXTRA_COMMAND="-l --verbosity"
+MAPDATA_NAME_WITH_SUFFIX=map
 
 _sig() {
   kill -TERM $child 2>/dev/null
@@ -7,19 +10,41 @@ _sig() {
 
 if [ "$1" = 'routed_startup' ]; then
   trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
-  ./osrm-routed $DATA_PATH/$2.osrm -a MLD --max-table-size 8000 &
+
+  TRAFFIC_FILE=traffic.csv
+  TRAFFIC_PROXY_IP=${2:-"10.189.102.81"}
+
+  cd ${DATA_PATH}
+  ${BUILD_PATH}/osrm_traffic_updater -c ${TRAFFIC_PROXY_IP} -d=false -f ${TRAFFIC_FILE}
+  ls -lh
+  ${BUILD_PATH}/osrm-customize ${MAPDATA_NAME_WITH_SUFFIX}.osrm  --segment-speed-file ${TRAFFIC_FILE} ${OSRM_EXTRA_COMMAND}
+  ${BUILD_PATH}/osrm-routed ${MAPDATA_NAME_WITH_SUFFIX}.osrm -a MLD --max-table-size 8000 &
   child=$!
   wait "$child"
+
 elif [ "$1" = 'compile_mapdata' ]; then
   trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
-  if [ ! -f $DATA_PATH/$2.osrm ]; then
-    if [ ! -f $DATA_PATH/$2.osm.pbf ]; then
-      curl $3 > $DATA_PATH/$2.osm.pbf
-    fi
-    ./osrm-extract $DATA_PATH/$2.osm.pbf -p profiles/car.lua
-    ./osrm-partition $DATA_PATH/$2.osrm
-    ./osrm-customize $DATA_PATH/$2.osrm
-  fi  
+
+  PBF_FILE_URL=${2}
+  KEEP_COMPILED_DATA=${3:-"false"}
+
+  curl ${PBF_FILE_URL} > $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osm.pbf
+  ${BUILD_PATH}/osrm-extract $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osm.pbf -p profiles/car.lua ${OSRM_EXTRA_COMMAND}
+  ${BUILD_PATH}/osrm-partition $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osrm ${OSRM_EXTRA_COMMAND}
+  ${BUILD_PATH}/osrm-customize $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osrm ${OSRM_EXTRA_COMMAND}
+  
+  # clean source pbf and temp .osrm
+  rm -f $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osm.pbf
+  rm -f $DATA_PATH/${MAPDATA_NAME_WITH_SUFFIX}.osrm
+
+  #TODO: package and publish compiled mapdata 
+
+
+  # rm compiled data if not needed
+  if [ ${KEEP_COMPILED_DATA} != "true" ]; then
+    rm -f $DATA_PATH/*
+  fi
+
 else
   exec "$@"
 fi
