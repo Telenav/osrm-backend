35a86484c3d0262bfc1db022686dc83a81a36bf4
Jay Zhang <wangyoucao577@gmail.com>
2019-11-05 09:53:11 +0800

    feat: dump file name flag, function not implemented yet

--- integration/cmd/trafficproxyclient/flags.go
+++ integration/cmd/trafficproxyclient/flags.go
@@ -8,10 +8,12 @@ var flags struct {
 	streamingDelta bool
 	wayIDsFlag     wayIDsFlag
 	blockingOnly   bool
+	dumpFile       string
 }
 
 func init() {
 	flag.BoolVar(&flags.streamingDelta, "delta", false, "Whether receive traffic data by streaming delta mode. Defaultly false means get traffic data once.")
 	flag.Var(&flags.wayIDsFlag, "ways", "wayIDs for querying traffic. Use ',' split if more than one wayID. Positive value means forward, negative value means backward. E.g. '829733412,-104489539'.")
 	flag.BoolVar(&flags.blockingOnly, "blocking-only", false, "Only use blocking only(blocking flow or blocking incident) live traffic.")
+	flag.StringVar(&flags.dumpFile, "dumpfile", "", "Dump file name of flows,incidents. Flows,incident will be dumped to files(xxx_flows.csv,xxx_incidents.csv) instead of stdout.")
 }
