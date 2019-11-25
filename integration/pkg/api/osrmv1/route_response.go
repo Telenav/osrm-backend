647d354d1f69f720156cf9fed5f80d0eebf7fa25
Jay Zhang <wangyoucao577@gmail.com>
2019-11-25 15:33:55 +0800

    feat: omit empty for json response

--- integration/pkg/api/osrmv1/route_response.go
+++ integration/pkg/api/osrmv1/route_response.go
@@ -2,31 +2,31 @@ package osrmv1
 
 // RouteResponse represent OSRM api v1 route response.
 type RouteResponse struct {
-	Code        string     `json:"code"`
-	Message     string     `json:"message,omitempty"`
-	DataVersion string     `json:"data_version,omitempty"`
-	Routes      []Route    `json:"routes,omitempty"`
-	Waypoints   []Waypoint `json:"waypoints,omitempty"`
+	Code        string      `json:"code"`
+	Message     string      `json:"message,omitempty"`
+	DataVersion string      `json:"data_version,omitempty"`
+	Routes      []*Route    `json:"routes,omitempty"`
+	Waypoints   []*Waypoint `json:"waypoints,omitempty"`
 }
 
 // Route represents a route through (potentially multiple) waypoints.
 type Route struct {
-	Distance   float32    `json:"distance"`
-	Duration   float32    `json:"duration"`
-	Geometry   string     `json:"geometry"`
-	Weight     float32    `json:"weight"`
-	WeightName string     `json:"weight_name"`
-	Legs       []RouteLeg `json:"legs,omitempty"`
+	Distance   float32     `json:"distance"`
+	Duration   float32     `json:"duration"`
+	Geometry   string      `json:"geometry"`
+	Weight     float32     `json:"weight"`
+	WeightName string      `json:"weight_name"`
+	Legs       []*RouteLeg `json:"legs,omitempty"`
 }
 
 // RouteLeg represents a route between two waypoints.
 type RouteLeg struct {
-	Distance   float32     `json:"distance"`
-	Duration   float32     `json:"duration"`
-	Weight     float32     `json:"weight"`
-	Summary    string      `json:"summary"`
-	Steps      []RouteStep `json:"steps,omitempty"`
-	Annotation Annotation  `json:"annotation"`
+	Distance   float32      `json:"distance"`
+	Duration   float32      `json:"duration"`
+	Weight     float32      `json:"weight"`
+	Summary    string       `json:"summary"`
+	Steps      []*RouteStep `json:"steps,omitempty"`
+	Annotation Annotation   `json:"annotation"`
 }
 
 // RouteStep A step consists of a maneuver such as a turn or merge, followed by a distance of travel along a single way to the subsequent step.
