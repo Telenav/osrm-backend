915644349f4e8eb8e7ae0d7ffc58416259bdec4f
Jay Zhang <wangyoucao577@gmail.com>
2019-11-26 23:23:04 +0800

    test: ut for incidents cache indexed by edge

--- integration/trafficcache/incidentscache/incidents_test.go
+++ integration/trafficcache/incidentscache/incidents_test.go
@@ -3,6 +3,7 @@ package incidentscache
 import (
 	"testing"
 
+	"github.com/Telenav/osrm-backend/integration/graph"
 	proxy "github.com/Telenav/osrm-backend/integration/pkg/trafficproxy"
 )
 
@@ -52,51 +53,97 @@ func TestIncidentsCache(t *testing.T) {
 		},
 	}
 
+	wayid2NodeIDsMapping := wayID2NodeIDs{
+		1204020274: []int64{123456789001, 123456789002, 123456789003, 123456789004},
+		100663296:  []int64{123456789011, 123456789012, 123456789003},
+		19446119:   []int64{123456789021, 123456789002, 123456789023, 123456789024, 123456789025, 123456789026},
+		1204020275: []int64{123456789031, 123456789032, 123456789033, 123456789034},
+		100643296:  []int64{123456789041, 123456789042, 123456789043},
+	}
+
 	cache := New()
+	cacheWithEdgeIndexing := NewWithEdgeIndexing(wayid2NodeIDsMapping)
 
 	// update
 	cache.Update(newIncidentsResponses(presetIncidents, proxy.Action_UPDATE))
+	cacheWithEdgeIndexing.Update(newIncidentsResponses(presetIncidents, proxy.Action_UPDATE))
 	expectIncidentsCount := 2
-	if cache.Count() != expectIncidentsCount {
-		t.Errorf("expect cached incidents count %d but got %d", expectIncidentsCount, cache.Count())
+	if cache.Count() != expectIncidentsCount || cacheWithEdgeIndexing.Count() != expectIncidentsCount {
+		t.Errorf("expect cached incidents count %d but got %d,%d", expectIncidentsCount, cache.Count(), cacheWithEdgeIndexing.Count())
 	}
 	expectAffectedWaysCount := 4 // only store blocked incidents
-	if cache.AffectedWaysCount() != expectAffectedWaysCount {
-		t.Errorf("expect cached incidents affect ways count %d but got %d", expectAffectedWaysCount, cache.AffectedWaysCount())
+	if cache.AffectedWaysCount() != expectAffectedWaysCount || cacheWithEdgeIndexing.AffectedWaysCount() != expectAffectedWaysCount {
+		t.Errorf("expect cached incidents affect ways count %d but got %d,%d", expectAffectedWaysCount, cache.AffectedWaysCount(), cacheWithEdgeIndexing.AffectedWaysCount())
+	}
+	expectAffectedEdgesCount := 12
+	if cacheWithEdgeIndexing.AffectedEdgesCount() != expectAffectedEdgesCount {
+		t.Errorf("expect cached incidents affect edges count %d but got %d", expectAffectedEdgesCount, cacheWithEdgeIndexing.AffectedEdgesCount())
 	}
 
 	// query expect sucess
 	inCacheWayIDs := []int64{100663296, 19446119, -1204020275, 100643296}
 	for _, wayID := range inCacheWayIDs {
-		if !cache.WayBlockedByIncident(wayID) {
+		if !cache.WayBlockedByIncident(wayID) || !cacheWithEdgeIndexing.WayBlockedByIncident(wayID) {
 			t.Errorf("wayID %d, expect blocked by incident but not", wayID)
+		}
+		edges := wayid2NodeIDsMapping.WayID2Edges(wayID)
+		for _, e := range edges {
+			if !cacheWithEdgeIndexing.EdgeBlockedByIncident(e) {
+				t.Errorf("edge %v, expect blocked by incident but not", e)
+			}
+		}
+		if b, i := cacheWithEdgeIndexing.EdgesBlockedByIncidents(edges); !b || i != 0 {
+			t.Errorf("edges %v, expect blocked by incidents but not", edges)
 		}
 	}
 
 	// query expect fail
 	notInCacheWayIDs := []int64{0, 100000, -23456789723}
 	for _, wayID := range notInCacheWayIDs {
-		if cache.WayBlockedByIncident(wayID) {
+		if cache.WayBlockedByIncident(wayID) || cacheWithEdgeIndexing.WayBlockedByIncident(wayID) {
 			t.Errorf("wayID %d, expect not blocked by incident but yes", wayID)
+		}
+	}
+	notInCacheEdges := []graph.Edge{
+		graph.Edge{},
+		graph.Edge{From: 12345, To: 123456789004},
+		graph.Edge{From: 1000000000, To: 123456789012},
+		graph.Edge{From: 123456789001, To: 123456789002},
+	}
+	for _, e := range notInCacheEdges {
+		if cacheWithEdgeIndexing.EdgeBlockedByIncident(e) {
+			t.Errorf("edge %v, expect not blocked by incident but yes", e)
 		}
 	}
 
 	// delete
 	deleteIncidents := presetIncidents[:2]
 	cache.Update(newIncidentsResponses(deleteIncidents, proxy.Action_DELETE))
+	cacheWithEdgeIndexing.Update(newIncidentsResponses(deleteIncidents, proxy.Action_DELETE))
 	expectIncidentsCount = 1
-	if cache.Count() != expectIncidentsCount {
-		t.Errorf("expect after delete, cached incidents count %d but got %d", expectIncidentsCount, cache.Count())
+	if cache.Count() != expectIncidentsCount || cacheWithEdgeIndexing.Count() != expectIncidentsCount {
+		t.Errorf("expect after delete, cached incidents count %d but got %d,%d", expectIncidentsCount, cache.Count(), cacheWithEdgeIndexing.Count())
 	}
 	expectAffectedWaysCount = 3 // only store blocked incidents
-	if cache.AffectedWaysCount() != expectAffectedWaysCount {
-		t.Errorf("expect cached incidents affect ways count %d but got %d", expectAffectedWaysCount, cache.AffectedWaysCount())
+	if cache.AffectedWaysCount() != expectAffectedWaysCount || cacheWithEdgeIndexing.AffectedWaysCount() != expectAffectedWaysCount {
+		t.Errorf("expect cached incidents affect ways count %d but got %d,%d", expectAffectedWaysCount, cache.AffectedWaysCount(), cacheWithEdgeIndexing.AffectedWaysCount())
+	}
+	expectAffectedEdgesCount = 7
+	if cacheWithEdgeIndexing.AffectedEdgesCount() != expectAffectedEdgesCount {
+		t.Errorf("expect cached incidents affect edges count %d but got %d", expectAffectedEdgesCount, cacheWithEdgeIndexing.AffectedEdgesCount())
 	}
 
 	// clear
 	cache.Clear()
-	if cache.Count() != 0 {
-		t.Errorf("expect cached incidents count 0 due to clear but got %d", cache.Count())
+	cacheWithEdgeIndexing.Clear()
+	if cache.Count() != 0 || cacheWithEdgeIndexing.Count() != 0 {
+		t.Errorf("expect cached incidents count 0 due to clear but got %d,%d", cache.Count(), cacheWithEdgeIndexing.Count())
+	}
+	if cache.AffectedWaysCount() != 0 || cacheWithEdgeIndexing.AffectedWaysCount() != 0 {
+		t.Errorf("expect cached incidents affect ways count 0 but got %d,%d", cache.AffectedWaysCount(), cacheWithEdgeIndexing.AffectedWaysCount())
+	}
+	if cacheWithEdgeIndexing.AffectedEdgesCount() != 0 {
+		t.Errorf("expect cached incidents affect edges count 0 but got %d", cacheWithEdgeIndexing.AffectedEdgesCount())
 	}
 
 }
