d15e58a1660dcf6924bb707d06c9c29765597406
CoderBear801 <coderxiong@gmail.com>
2020-02-18 12:10:44 -0800

    fix: fix oasis/main based on api change. issue: https://github.com/Telenav/osrm-backend/issues/143

--- integration/cmd/oasis/flags.go
+++ integration/cmd/oasis/flags.go
@@ -5,11 +5,17 @@ import (
 )
 
 var flags struct {
-	listenPort          int
-	osrmBackendEndpoint string
+	listenPort           int
+	osrmBackendEndpoint  string
+	tnSearchEndpoint     string
+	tnSearchAPIKey       string
+	tnSearchAPISignature string
 }
 
 func init() {
 	flag.IntVar(&flags.listenPort, "p", 8090, "Listen port.")
-	flag.StringVar(&flags.osrmBackendEndpoint, "osrm", "", "Backend OSRM-backend endpoint")
+	flag.StringVar(&flags.osrmBackendEndpoint, "osrm", "", "OSRM-backend endpoint")
+	flag.StringVar(&flags.osrmBackendEndpoint, "search", "", "TN-Search-backend endpoint")
+	flag.StringVar(&flags.osrmBackendEndpoint, "searchApiKey", "", "API key for TN-Search-backend")
+	flag.StringVar(&flags.osrmBackendEndpoint, "searchApiSignature", "", "API Signature for  TN-Search-backend")
 }
