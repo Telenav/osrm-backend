01aa0c15139efae739c7906cb5c717274757cbfa
CoderBear801 <coderxiong@gmail.com>
2020-02-19 13:41:55 -0800

    fix: add doc file and remove untested code. issue: https://github.com/Telenav/osrm-backend/issues/143

--- integration/oasis/stationfinder/stations_iterator.go
+++ integration/oasis/stationfinder/stations_iterator.go
@@ -3,16 +3,3 @@ package stationfinder
 type nearbyStationsIterator interface {
 	iterateNearbyStations() <-chan ChargeStationInfo
 }
-
-// ChargeStationInfo defines charge station information
-type ChargeStationInfo struct {
-	ID       string
-	Location StationCoordinate
-	err      error
-}
-
-// StationCoordinate represents location information
-type StationCoordinate struct {
-	Lat float64
-	Lon float64
-}
