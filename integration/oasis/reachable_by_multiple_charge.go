d54dad4458b8fd6afc46159e426d96de42da4f16
CoderBear801 <coderxiong@gmail.com>
2020-02-27 16:42:09 -0800

    fix: add unit test for function findChargeLocation4Route issue: https://github.com/Telenav/osrm-backend/issues/143

--- integration/oasis/reachable_by_multiple_charge.go
+++ integration/oasis/reachable_by_multiple_charge.go
@@ -11,7 +11,7 @@ import (
 )
 
 // For each route response, will generate an array of *stationfinder.StationCoordinate
-// It will contains: start point, first charge point(could also be start point), second charge point, ..., end point
+// Each array contains: start point, first charge point(could also be start point), second charge point, ..., end point
 func chargeLocationSelection(oasisReq *oasis.Request, routeResp *route.Response) [][]*stationfinder.StationCoordinate {
 	results := [][]*stationfinder.StationCoordinate{}
 	for _, route := range routeResp.Routes {
@@ -30,7 +30,7 @@ func chargeLocationSelection(oasisReq *oasis.Request, routeResp *route.Response)
 			currEnergy = oasisReq.MaxRange
 		}
 
-		result, currEnergy = findChargeLocation4Route(oasisReq, route, result, currEnergy)
+		result, currEnergy = findChargeLocation4Route(route, result, currEnergy, oasisReq.PreferLevel, oasisReq.MaxRange)
 		if len(result) != 0 {
 			result = append(result, &stationfinder.StationCoordinate{
 				Lat: oasisReq.Coordinates[1].Lat,
@@ -41,26 +41,26 @@ func chargeLocationSelection(oasisReq *oasis.Request, routeResp *route.Response)
 	return results
 }
 
-func findChargeLocation4Route(oasisReq *oasis.Request, route *route.Route, result []*stationfinder.StationCoordinate, currEnergy float64) ([]*stationfinder.StationCoordinate, float64) {
+func findChargeLocation4Route(route *route.Route, result []*stationfinder.StationCoordinate, currEnergy, preferLevel, maxRange float64) ([]*stationfinder.StationCoordinate, float64) {
 	for _, leg := range route.Legs {
 		for _, step := range leg.Steps {
-			if currEnergy-step.Distance < oasisReq.PreferLevel {
-				coords, _, err := polyline.DecodeCoords([]byte(route.Geometry))
+			if (currEnergy - step.Distance) < preferLevel {
+				coords, _, err := polyline.DecodeCoords([]byte(step.Geometry))
 				if err != nil {
-					glog.Error("Incorrect geometry encoding string from route response, error=%v", err)
+					glog.Errorf("Incorrect geometry encoding string from route response, error=%v", err)
 					return nil, 0.0
 				}
 
 				tmp := 0.0
 				for i := 0; i < len(coords)-1; i++ {
 					tmp += haversine.GreatCircleDistance(coords[i][0], coords[i][1], coords[i+1][0], coords[i+1][1])
-					if currEnergy-tmp < oasisReq.PreferLevel {
-						currEnergy = oasisReq.MaxRange
+					if currEnergy-tmp < preferLevel {
+						currEnergy = maxRange
 						result = append(result, &stationfinder.StationCoordinate{
 							Lat: coords[i][0],
 							Lon: coords[i][1]})
 
-						if currEnergy > (step.Distance - tmp + oasisReq.PreferLevel) {
+						if currEnergy > (step.Distance - tmp + preferLevel) {
 							currEnergy -= step.Distance - tmp
 							break
 						} else {
