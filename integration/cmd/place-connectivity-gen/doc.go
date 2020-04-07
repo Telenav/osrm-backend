840da12ca4ec609588e14308bc8c2add01bf74c6
CoderBear801 <coderxiong@gmail.com>
2020-04-07 09:57:09 -0700

    fix: update document.

--- integration/cmd/place-connectivity-gen/doc.go
+++ integration/cmd/place-connectivity-gen/doc.go
@@ -1,21 +1,20 @@
 // Package main contains the tool of chargestation-connectivity generator
 //
 // stage 1:
-// inputs is json file
+// inputs is json file(Example: https://github.com/Telenav/osrm-backend/blob/2b28e5b62de7fbf35163698966ee005683b1c8a3/integration/service/spatialindexer/poiloader/sample_input.json#L1)
 // => convert to slice of [id:string,location: lat,lon]
-// => calculate cellids for each point(for all levels)
+// => calculate cellids for each location(for all levels)
 // => build revese index for cellID -> ids
 //
 // stage 2:
-// => iterate each point
+// => iterate each place id
 // => generate a circle(s2::cap), find all cellids intersect with that circle
-// => retrieve all ids
-// => generate result of id(from), ids(all ids in certain distance)
+// => retrieve all place ids in those cellids
+// => generate result of place id(from), place ids
 //
 // stage 3:
-// => load data from file
-// => for each line, its formid and all other ids
-// => calculate distance between fromid and all other ids
-// => sort result based on distance and write back to file
+// => load previous result from file
+// => rank place ids by calculating distance between center place id and all others
+// => Write results to file
 //
 package main
